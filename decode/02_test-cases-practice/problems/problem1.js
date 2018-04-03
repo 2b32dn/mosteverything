4fcd8051c4162f441518a8257eabdb3780c783af
Dickson Lee <dicksont_lee@hotmail.com>
2018-04-03 13:57:03 -0400

    ass2prblm1

--- decode/02_test-cases-practice/problems/problem1.js
+++ decode/02_test-cases-practice/problems/problem1.js
@@ -2,20 +2,34 @@ var assert = require('assert');
 
 // we need 5 test cases. I provided 1 input
 let inputs = [
-  ""
+  "",
+  "abc",
+  "def",
+  "ghi",
+  "jkl"
 ]
 
 let outputs = [
-  
+  undefined,
+  "a",
+  "d",
+  "g",
+  "j"
 ]
 
 // Make this function return the first letter of the string that is passed to it. If the string does not have a first letter, return undefined
 function f(str) {
-    
+  for(var i = 0; i<=inputs.length;i++){
+    if(outputs[i] === undefined){
+      return undefined;
+    } else{
+      return inputs[i].charAt(0);
+    }
+  }
 }
 
 function runTest(i) {
-    var expected = outputs[i];
+    var expected = f(outputs[i]);
     var actual = f(inputs[i]);
     assert.deepEqual(actual, expected);
 }
