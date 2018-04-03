00c1f9cf7a763ee124950f52b0628cffc0483ea3
Dickson Lee <dicksont_lee@hotmail.com>
2018-04-03 14:34:45 -0400

    ass2prblm2

--- decode/02_test-cases-practice/problems/problem2.js
+++ decode/02_test-cases-practice/problems/problem2.js
@@ -2,21 +2,35 @@ var assert = require('assert');
 
 // we need 5 test cases.
 let inputs = [
-
+    "",
+    "qddw",
+    "as",
+    "zx",
+    "ecr"
 ]
 
 let outputs = [
-  
+  undefined,
+  "w",
+  "s",
+  "x",
+  "r"
 ]
 
 // Make this function return the last letter of the string that is passed to it. If the string does not have a last letter, return undefined
 function f(str) {
-    
+    if(str === ""){
+        return undefined;
+    } else{
+        return str.slice(str.length - 1);
+    }
 }
 
 function runTest(i) {
     var expected = outputs[i];
+    console.log(expected);
     var actual = f(inputs[i]);
+    console.log(actual);
     assert.deepEqual(actual, expected);
 }
 
