a5ad5baba6088b72d944a0134efc47afe496a914
Dickson Lee <dicksont_lee@hotmail.com>
2018-04-03 18:34:36 -0400

    ass2prblm8

--- decode/02_test-cases-practice/problems/problem8.js
+++ decode/02_test-cases-practice/problems/problem8.js
@@ -2,11 +2,19 @@ var assert = require('assert');
 
 // we need 5 test cases. 
 let inputs = [
-  
+  "kayak",
+  "racecar",
+  "dickson",
+  "lee",
+  "how u doing"
 ]
 
 let outputs = [
-  
+  "kayak",
+  "racecar",
+  "noskcid",
+  "eel",
+  "gniod u woh"
 ]
 
 /*
@@ -14,7 +22,9 @@ Make this function return the input string, reversed. For example "hello" would 
 You must use a for loop for this exercise.
 */
 function f(str) {
-    
+    for(var i = 0; i<=str.length;i++){
+        return str.split("").reverse().join("");
+    }
 }
 
 function runTest(i) {
